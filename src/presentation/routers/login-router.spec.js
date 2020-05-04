499b3378abb7643c1316a6b8394358c3835e3936
ianlavapiez <ianlavapiez@gmail.com>
2020-05-04 23:12:09 +0800

    lint-staged: added test:staged npm script to test first before committing

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,6 +1,6 @@
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
         statusCode: 400
       }
@@ -14,6 +14,17 @@ describe('Login Router', () => {
     const httpRequest = {
       body: {
         password: 'any_password'
+      }
+    }
+    const httpResponse = systemUnitTest.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('should return 400 if no password has provided', () => {
+    const systemUnitTest = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'any_email@gmail.com'
       }
     }
     const httpResponse = systemUnitTest.route(httpRequest)
