99cb0d2896a3b47306c51d3db9b218829d997be4
ianlavapiez <ianlavapiez@gmail.com>
2020-05-05 13:04:58 +0800

    jest: moved system unit test creation to a factory helper method

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,9 +1,13 @@
 const LoginRouter = require('./login-router')
 const MissingParamError = require('../helpers/missing-param-error')
 
+const makeSystemUnitTest = () => {
+  return new LoginRouter()
+}
+
 describe('Login Router', () => {
   test('should return 400 if no email has provided', () => {
-    const systemUnitTest = new LoginRouter()
+    const systemUnitTest = makeSystemUnitTest()
     const httpRequest = {
       body: {
         password: 'any_password'
@@ -16,7 +20,7 @@ describe('Login Router', () => {
   })
 
   test('should return 400 if no password has provided', () => {
-    const systemUnitTest = new LoginRouter()
+    const systemUnitTest = makeSystemUnitTest()
     const httpRequest = {
       body: {
         email: 'any_email_provided@gmail.com'
@@ -29,14 +33,14 @@ describe('Login Router', () => {
   })
 
   test('should return 500 if no httpRequest has provided', () => {
-    const systemUnitTest = new LoginRouter()
+    const systemUnitTest = makeSystemUnitTest()
     const httpResponse = systemUnitTest.route()
 
     expect(httpResponse.statusCode).toBe(500)
   })
 
   test('should return 500 if no httpRequest has no body', () => {
-    const systemUnitTest = new LoginRouter()
+    const systemUnitTest = makeSystemUnitTest()
     const httpResponse = systemUnitTest.route({})
 
     expect(httpResponse.statusCode).toBe(500)
