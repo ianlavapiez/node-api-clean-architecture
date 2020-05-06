280914279a55a2b411a15bc7afecf00449aad92f
ianlavapiez <ianlavapiez@gmail.com>
2020-05-06 16:55:45 +0800

    jest: ensure AuthUseCase calls LoadUserByEmailRepository with correct email

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,5 +1,9 @@
 const { MissingParamError } = require('../../utils/errors')
 class AuthUseCase {
+  constructor (loadUserByEmailRepositorySpy) {
+    this.loadUserByEmailRepositorySpy = loadUserByEmailRepositorySpy
+  }
+
   async auth (email, password) {
     if (!email) {
       throw new MissingParamError('email')
@@ -8,6 +12,8 @@ class AuthUseCase {
     if (!password) {
       throw new MissingParamError('password')
     }
+
+    this.loadUserByEmailRepositorySpy.load(email)
   }
 }
 
@@ -24,5 +30,19 @@ describe('Auth UseCase', () => {
     const promise = systemUnderTest.auth('any_email@gmail.com')
 
     expect(promise).rejects.toThrow(new MissingParamError('password'))
+  })
+
+  test('should  call LoadUserByEmailRepository with correct email', async () => {
+    class LoadUserByEmailRepositorySpy {
+      async load (email) {
+        this.email = email
+      }
+    }
+
+    const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy()
+    const systemUnderTest = new AuthUseCase(loadUserByEmailRepositorySpy)
+    systemUnderTest.auth('any_email@gmail.com', 'any_password')
+
+    expect(loadUserByEmailRepositorySpy.email).toBe('any_email@gmail.com')
   })
 })
