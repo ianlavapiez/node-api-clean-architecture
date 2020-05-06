a7ff901af78493890a15e39468fedb4dbdc9dd6e
ianlavapiez <ianlavapiez@gmail.com>
2020-05-06 15:07:50 +0800

    jest: added jest configurations

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -29,6 +29,7 @@ module.exports = class LoginRouter {
 
       return HttpResponse.ok({ accessToken })
     } catch (error) {
+      console.error(error)
       return HttpResponse.serverError()
     }
   }
