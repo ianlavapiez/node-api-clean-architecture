ef973f061a8c7259d6f73994d01af7aa3e39728f
ianlavapiez <ianlavapiez@gmail.com>
2020-05-06 22:43:29 +0800

    jest: ensure TokenGenerator will call JWT with correct values

---
+++ __mocks__/jsonwebtoken.js
@@ -0,0 +1,12 @@
+module.exports = {
+  token: 'any_token',
+  id: '',
+  secret: '',
+
+  sign (id, secret) {
+    this.id = id
+    this.secret = secret
+
+    return this.token
+  }
+}
--- src/utils/token-generator.spec.js
+++ src/utils/token-generator.spec.js
@@ -1,14 +1,42 @@
+const jwt = require('jsonwebtoken')
+
 class TokenGenerator {
-  async generate (id) {
-    return null
+  constructor (secret) {
+    this.secret = secret
   }
+
+  async generate (id) {
+    return jwt.sign(id, this.secret)
+  }
+}
+
+const makeSystemUnderTest = () => {
+  return new TokenGenerator('secret')
 }
 
 describe('Token Generator', () => {
   test('should return null if JWT returns null', async () => {
-    const systemUnderTest = new TokenGenerator()
+    const systemUnderTest = makeSystemUnderTest()
+
+    jwt.token = null
+
     const token = await systemUnderTest.generate('any_id')
 
     expect(token).toBeNull()
+  })
+
+  test('should return a token if JWT returns token', async () => {
+    const systemUnderTest = makeSystemUnderTest()
+    const token = await systemUnderTest.generate('any_token')
+
+    expect(token).toBe(jwt.token)
+  })
+
+  test('should call JWT with correct values', async () => {
+    const systemUnderTest = makeSystemUnderTest()
+    await systemUnderTest.generate('any_id')
+
+    expect(jwt.id).toBe('any_id')
+    expect(jwt.secret).toBe(systemUnderTest.secret)
   })
 })
