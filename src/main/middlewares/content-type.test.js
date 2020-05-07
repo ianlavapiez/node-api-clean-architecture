416838ff1892480bf7cac481c0b81bd69a0549fe
ianlavapiez <ianlavapiez@gmail.com>
2020-05-07 17:35:51 +0800

    test: reset jest modules in content-type

--- src/main/middlewares/content-type.test.js
+++ src/main/middlewares/content-type.test.js
@@ -1,7 +1,13 @@
 const request = require('supertest')
-const app = require('../config/app')
 
 describe('Content-Type middleware', () => {
+  let app
+
+  beforeEach(() => {
+    jest.resetModules()
+    app = require('../config/app')
+  })
+
   test('should return json content-type as default', async () => {
     app.get('/test_content_type', (req, res) => {
       res.send('')
