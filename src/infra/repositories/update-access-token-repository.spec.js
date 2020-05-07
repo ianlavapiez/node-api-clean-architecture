1e391976555f3849c414ac0ca97f898b4bb95d5c
ianlavapiez <ianlavapiez@gmail.com>
2020-05-07 14:34:10 +0800

    infra: moved updateAccessTokenRepository to its own file

--- src/infra/repositories/update-access-token-repository.spec.js
+++ src/infra/repositories/update-access-token-repository.spec.js
@@ -1,30 +1,8 @@
 const MongoHelper = require('../helpers/mongo-helper')
 const MissingParamError = require('../../utils/errors/missing-param-error')
+const UpdateAccessTokenRepository = require('./update-access.token-repository')
 
 let db
-
-class UpdateAccessTokenRepository {
-  constructor (userModel) {
-    this.userModel = userModel
-  }
-
-  async update (userId, accessToken) {
-    if (!userId) {
-      throw new MissingParamError('userId')
-    }
-
-    if (!accessToken) {
-      throw new MissingParamError('accessToken')
-    }
-    await this.userModel.updateOne({
-      _id: userId
-    }, {
-      $set: {
-        accessToken
-      }
-    })
-  }
-}
 
 const makeSystemUnderTest = () => {
   const userModel = db.collection('users')
