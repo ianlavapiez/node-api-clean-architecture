0b99adce10fdd8de3b252b31d543036e974bcd3a
ianlavapiez <ianlavapiez@gmail.com>
2020-05-07 17:44:57 +0800

    jest: disconnect mongodb after MongoHelper tests

--- src/infra/helpers/mongo-helper.spec.js
+++ src/infra/helpers/mongo-helper.spec.js
@@ -1,10 +1,15 @@
-const MongoHelper = require('./mongo-helper')
+const systemUnderTest = require('./mongo-helper')
 
 describe('Mongo Helper', () => {
-  test('should reconnect when getDb is invoked and client is disconnected', async () => {
-    const systemUnderTest = MongoHelper
-
+  beforeAll(async () => {
     await systemUnderTest.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await systemUnderTest.disconnect()
+  })
+
+  test('should reconnect when getDb is invoked and client is disconnected', async () => {
     expect(systemUnderTest.db).toBeTruthy()
 
     await systemUnderTest.disconnect()
