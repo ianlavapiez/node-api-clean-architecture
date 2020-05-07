8d194b2a4e59e4d09048d8abbb83ed574cd632a9
ianlavapiez <ianlavapiez@gmail.com>
2020-05-07 17:17:03 +0800

    middlewares: moved CORS to its own file

--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -11,16 +11,4 @@ describe('App Setup', () => {
 
     expect(res.headers['x-powered-by']).toBeUndefined()
   })
-
-  test('should enable CORS', async () => {
-    app.get('/test_cors', (req, res) => {
-      res.send('')
-    })
-
-    const res = await request(app).get('/test_cors')
-
-    expect(res.headers['access-control-allow-origin']).toBe('*')
-    expect(res.headers['access-control-allow-methods']).toBe('*')
-    expect(res.headers['access-control-allow-headers']).toBe('*')
-  })
 })
